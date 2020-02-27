6b8bad591bbd494438fb53ee23eb40be81352fc8
wh0dat <whodat@remainsilent.net>
2020-02-27 08:24:16 +0530

    hannah: Rework check_device.sh
    Signed-off-by: wh0dat <whodat@remainsilent.net>
    Change-Id: Ibd26f16f4204e60d9389f9e50f2019587e8aa898

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -2,18 +2,18 @@
 
 sku=`getprop ro.boot.hardware.sku`
 
-if [ "$sku" = "XT1924-1" ] || [ "$sku" = "XT1924-3" ] || [ "$sku" = "XT1924-4" ] || [ "$sku" = "XT1924-5" ]; then
+if [ "$sku" = "XT1924-7" ] || [ "$sku" = "XT1924-8" ]; then
+    rm -rf /system/priv-app/MotoDoze
+    rm /vendor/etc/audio_platform_info_ahannah.xml
+    rm /vendor/etc/mixer_paths_ahannah.xml
+    rm /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf
+else
     mv /vendor/etc/audio_platform_info_ahannah.xml /vendor/etc/audio_platform_info.xml
     mv /vendor/etc/mixer_paths_ahannah.xml /vendor/etc/mixer_paths.xml
     mv /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf /vendor/etc/sensors/sensor_def_qcomdev.conf
     rm -rf /vendor/app/LineageActions
     rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
     rm /vendor/etc/permissions/android.hardware.sensor.gyroscope.xml
-else
-    rm -rf /system/priv-app/MotoDoze
-    rm /vendor/etc/audio_platform_info_ahannah.xml
-    rm /vendor/etc/mixer_paths_ahannah.xml
-    rm /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf
 fi
 
     # Replace EGL for rhannah variants
