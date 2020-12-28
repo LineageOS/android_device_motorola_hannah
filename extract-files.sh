4005f255baaa270c47195cf24e4a83d48ad47979
Bruno Martins <bgcngm@gmail.com>
2020-12-28 10:34:04 +0000

    hannah: Align with extraction-utils templates
    Change-Id: Ifbf18f79a1d0f173cb5b83bdf0178db2a5f1373e

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,11 +14,8 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=hannah
 export DEVICE_COMMON=hannah-common
 export VENDOR=motorola
-
-export DEVICE_BRINGUP_YEAR=2019
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
